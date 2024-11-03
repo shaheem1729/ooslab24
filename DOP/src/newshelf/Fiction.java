a5313fb00b02bc04c4064188b9c898ec810211f9
shaheem1729 <122715172+shaheem1729@users.noreply.github.com>
2024-11-03 19:34:13 +0530

    Update Fiction.java

--- DOP/src/newshelf/Fiction.java
+++ DOP/src/newshelf/Fiction.java
@@ -1,8 +1,14 @@
 package newshelf;
 
-public record Fiction( String name) implements IBook {
+public record Fiction(String name) implements IBook<String> {
+
     @Override
     public String getTitle() {
         return name;
+    }
+
+    @Override
+    public int compareTo(IBook<String> other) {
+        return this.name.compareTo(other.getTitle());
     }
 }
