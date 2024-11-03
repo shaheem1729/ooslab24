f5beab88ec46bc04404e64143805e195ca0f2bf1
shaheem1729 <122715172+shaheem1729@users.noreply.github.com>
2024-11-03 19:34:58 +0530

    Update NewSelection.java

--- DOP/src/newshelf/NewSelection.java
+++ DOP/src/newshelf/NewSelection.java
@@ -2,27 +2,33 @@ package newshelf;
 
 public class NewSelection {
 
-  
     public static String getAgeOrTitle(Object o) {
-        if (o instanceof IBook book) {
-            if (book instanceof TextBook textBook) {
-                return textBook.subject();
-            } else if (book instanceof Fiction fiction) {
-                return fiction.name();
-            } else if (book instanceof Comic comic) {
-                return comic.title();
-            }
-        }
-        return "";
+        return switch (o) {
+            case TextBook(String subject) -> subject;
+            case Fiction(String name) -> name;
+            case Comic(String title, int ageRating) when ageRating >= 12 -> title;
+            case Comic(String title, int ageRating) -> "Comic not suitable for all ages";
+            case null, default -> "";
+        };
     }
 
     public static void main(String[] args) {
         TextBook textBook = new TextBook("Social Studies");
         Fiction fiction = new Fiction("Anthropologies");
         Comic comic = new Comic("He-Man", 15);
+        Comic youngComic = new Comic("Kids Adventures", 8);
 
         System.out.println("TextBook subject: " + getAgeOrTitle(textBook)); 
         System.out.println("Fiction name: " + getAgeOrTitle(fiction));    
         System.out.println("Comic title: " + getAgeOrTitle(comic));       
+        System.out.println("Young Comic title: " + getAgeOrTitle(youngComic));       
     }
 }
+
+interface IBook {}
+
+record TextBook(String subject) implements IBook {}
+
+record Fiction(String name) implements IBook {}
+
+record Comic(String title, int ageRating) implements IBook {}
