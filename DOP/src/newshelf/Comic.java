5ded7b11bccefdb6f7149c91df321dd9c62d3bb0
shaheem1729 <122715172+shaheem1729@users.noreply.github.com>
2024-11-03 19:33:48 +0530

    Update Comic.java

--- DOP/src/newshelf/Comic.java
+++ DOP/src/newshelf/Comic.java
@@ -1,8 +1,18 @@
 package newshelf;
 
-public record Comic(String title, int ageOfMainCharacter) implements IBook {
+public record Comic(String title, int ageRating) implements IBook<String> {
+
     @Override
     public String getTitle() {
         return title;
+    }
+
+    @Override
+    public int compareTo(IBook<String> other) {
+        return this.title.compareTo(other.getTitle());
+    }
+
+    public int ageRating() {
+        return ageRating;
     }
 }
